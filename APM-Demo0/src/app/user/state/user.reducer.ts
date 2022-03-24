ec79e93f228f9fed5e459b890bf6d786518a1bc5
Enrico Barbieri <enrico.barbieri@engagelabs.it>
2022-03-24 13:30:25 +0100

    Module 1 homeworks

---
+++ APM-Demo0/src/app/user/state/user.reducer.ts
@@ -0,0 +1,9 @@
+﻿import { ActionReducer, createAction, createReducer, on } from '@ngrx/store';
+
+export const userReducer: ActionReducer<{ maskUsername: boolean }> = createReducer(
+  {maskUsername: true},
+  on(createAction('[User] Mask username'), (state): any => ({
+    ...state,
+    maskUsername: !state.maskUsername
+  }))
+);
